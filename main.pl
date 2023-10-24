e12d8a58270bc08ebb73fe1c034cf22c6ea96cac
cristianromero97 <cristian.romero.m@usach.cl>
2023-10-24 18:24:23 -0400

    Avance

--- main.pl
+++ main.pl
@@ -370,22 +370,50 @@ systemAddChatbot(Sistema, Chatbot, NuevoSistema) :-
 
 %-----------------------------------------------------------------------------------------------------------------------------------
 
+%CONSTRUCTOR
+% Descripcion: Predicado para crear un usuario y asignarlo a una lista
+% Dominio: NombreUsuario x list.
+% Metas Primarias: usuario.
+% Metas Secundarias: usuario.
 usuario(NombreUsuario,[NombreUsuario]).
 
+%SELECTORES
+% Descripcion: Predicado que obtiene el NombreUsuario de un usuario dado.
+% Dominio: Usuario X Nombre.
+% Metas Primarias: getNombreUsuario.
+% Metas Secundarias: usuario.
 getNombreUsuario(Usuario,NombreUsuario):-
     usuario(NombreUsuario,Usuario).
 
+% Descripcion: Predicado que obtiene el usuario del chathistory dentro del sistema 1-Forma de hacerlo.
+% Dominio: Sistema X Usuario.
+% Metas Primarias: getUsuarios.
+% Metas Secundarias: usuario.
+getUsuarios(Sistema, Usuario):-
+    usuario([_|Usuario],Sistema).
+
+% Descripcion: Predicado que obtiene el usuario del chathistory dentro del sistema.
+% Dominio: Sistema X Usuario.
+% Metas Primarias: getUsuarios.
+% Metas Secundarias: findalll , usuario.
+getUsuarios(Sistema, Usuarios) :-
+    findall(NombreUsuario, usuario(NombreUsuario, Sistema), Usuarios).
+
+%MODIFICADOR
+% Descripcion: Predicado para cambiar el nombre del usuario dentro del sistema.
+% Dominio: Usuario X NuevoUsuario X NuevosUsuarios.
+% Metas Primarias: setNombreUsuario.
+% Metas Secundarias: usuario.
 setNombreUsuario(Usuario,NuevoNombre,NuevoUsuario):-
     usuario(_,Usuario),
     usuario(NuevoNombre,NuevoUsuario).
 
+% Descripcion: Predicado para añadir un nuevo usuario a una lista de usuarios.
+% Dominio: NuevoUsuuario X Usuarios X NuevosUsuarios
+% Metas Primarias: addUsuario.
+% Metas Secundarias: append.
 addUsuario(NuevoUsuario,Usuarios,NuevosUsuarios):-
     append(Usuarios,[NuevoUsuario],NuevosUsuarios).
-
-getUsuarios(Sistema, Usuario):-
-    usuario([_|Usuario],Sistema).
-getUsuarios(Sistema, Usuarios) :-
-    findall(NombreUsuario, usuario(NombreUsuario, Sistema), Usuarios).
 
 %-----------------------------------------------------------------------------------------------------------------------------------
 
@@ -449,9 +477,6 @@ systemLogout(Sistema, NuevoSistema) :-
     getUsuariosSystem(Sistema,Usuarios),
     getUsuarioLogeadoSystem(Sistema,UsuarioLogeado),
     chatHistory(Nombre,InitialChatbotCodeLink,Chatbot,Usuarios,[ ],Fecha, NuevoSistema),!.
-
-
-% Modificadores Usuarios y UsuariosLogeados
 
 setUsuarios(Sistema, NuevoUsuario, NuevoSistema) :-
     chatHistory(Nombre,InitialChatbotCodeLink,Chatbot,_,UsuarioLogeado,Fecha,Sistema).
