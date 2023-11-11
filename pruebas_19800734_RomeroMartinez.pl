07571bdc780dec865677201b3588c076a6a4f680
cristianromero97 <cristian.romero.m@usach.cl>
2023-11-10 23:05:43 -0400

    Pruebas

--- pruebas_19800734_RomeroMartinez.pl
+++ pruebas_19800734_RomeroMartinez.pl
@@ -99,18 +99,17 @@ chatbot(2,"Orientador Academico","Bienvenido\n ¿Que te gustaria estudiar?",1,[F
 %En este aspecto del codigo he puesto varios comentarios para poder usar correctamente los adduser, login y logout
 
 system("Chatbots Paradigmas",0,[CB0,CB0,CB1,CB2],S0),
-systemAddChatbot(S0,CB0,S99),    %Para no entomperser los demas predicados he puesto esta con una salida aleatoria, funciona si se cambia a la original
-systemAddUser(S0,"user1",S1),
-systemAddUser(S0,"user2",S2),
-systemAddUser(S0,"user2",S3),
-systemAddUser(S0,"user3",S4),
+systemAddChatbot(S0,CB0,S99), 
+systemAddUser(S0,"user1",S1),   
+systemAddUser(S1,"user2",S2),
+systemAddUser(S2,"user2",S3),
+systemAddUser(S3,"user3",S5),
 %systemLogin(S5,"user8",S6), %saltara un false, no esta registrado y no es igual al valor antes indicado en adduser
-systemLogin(S4,"user1",S5), %Para que funcione el valor del usuario debe ser igual al ultimo valor registrado por addUser
-%systemLogin(S4,"user3",S5), 
-%systemLogin(S5,S6),
-systemAddUser(S6,"user2",S7), %Aqui añado el usuario primero (con la unificacion esto hara que S6=S2=S3)
-systemLogin(S5,"user2",S6), %Dara el resultado debido a que es el mismo valor que el anterior
-systemLogout(S6,S7).
+systemLogin(S5,"user1",S6), %Para que funcione el valor del usuario debe ser igual al ultimo valor registrado por addUser
+%systemLogin(S6,"user3",S7), 
+%systemLogin(S7,"user2",S8),
+systemLogin(S7,"user2",S8), %Dara el resultado debido a que es el mismo valor que el anterior
+systemLogout(S7,S8).
 
 
 %Aparpatado de Ejemplos Extra
@@ -127,12 +126,12 @@ flowAddOption(F097,O95,F000),
 flowAddOption(F097,O96,F001),
 flowAddOption(FO96,O97,F002),
 %Ejemplos de Chatbots
-chatbot(1,"Chatbot 1 Electivo","Bienvenido ¿Que actividad prefieres?",3,[F95],CB001),
-chatbot(1,"Chatbot 2 Electivo","Bienvenido ¿Que actividad prefieres?",3,[F96],CB002),
-chatbot(1,"Chatbot 3 Electivo","Bienvenido ¿Que actividad prefieres?",3,[F95,F96],CB003),
-chatbotAddFlow(CB001,F96,CBC11), 
-chatbotAddFlow(CB002,F95,CBC12),
-chatbotAddFlow(CB003,F96,CBC13),
+chatbot(1,"Chatbot 1 Electivo","Bienvenido ¿Que actividad prefieres?",3,[F095],CB001),
+chatbot(1,"Chatbot 2 Electivo","Bienvenido ¿Que actividad prefieres?",3,[F096],CB002),
+chatbot(1,"Chatbot 3 Electivo","Bienvenido ¿Que actividad prefieres?",3,[F095,F096],CB003),
+chatbotAddFlow(CB001,F096,CBC11), 
+chatbotAddFlow(CB002,F095,CBC12),
+chatbotAddFlow(CB003,F096,CBC13),
 %Ejemplos de system
 system("Chatbot electivo de actividades random",1,[CB001],S10001),
 system("Chatbot electivo de actividades random",1,[CB002],S10002),
@@ -168,14 +167,64 @@ systemAddUser(S2, "user2", S3),
 %systemAddUser(S3, "user2", S4), %si esto se descomenta, debe dar false, porque es username duplicado
 systemAddUser(S3, "user3", S5),
 %systemLogin(S5, "user8", S6), %si esto se descomenta, debe dar false ;user8 no existe.
-%systemAddUser(S4,"user1",S5), %Segun mi implementacion el usuario añadido debe ser igual al usuario logeado, por ende el ultimo adduser debe ser igual al primer login
 systemLogin(S5, "user1", S7),
 %systemLogin(S7, "user2", S8), %si esto se descomenta, debe dar false, ya hay usuario con login
 systemLogout(S7, S9),
-%systemAddUser(S8,"user2",S9), %Complementando lo antes dicho en otra implementacion
 systemLogin(S9, "user2", S10).
 
 
+%SCRIPT N1 DE PRUEBAS VERSION DEFINITIVA
 
-
-
+option(1, "1) Viajar", 2, 1, ["viajar", "turistear", "conocer"], OP1),
+option(2, "2) Estudiar", 2, 1, ["estudiar", "aprender", "perfeccionarme"], OP2),
+flow(1, "flujo1", [OP1], F10),
+flowAddOption(F10, OP2, F11),
+% flowAddOption(F10, OP1, F12), %si esto se descomenta, debe dar false, porque es opción con id duplicada.
+chatbot(0, "Inicial", "Bienvenido\n¿Qué te gustaría hacer?", 1, [F11], CB0), %solo añade una ocurrencia de F11
+%Chatbot1
+option(1, "1) New York, USA", 1, 2, ["USA", "Estados Unidos", "New York"], OP3),
+option(2, "2) París, Francia", 1, 1, ["Paris", "Eiffel"], OP4),
+option(3, "3) Torres del Paine, Chile", 1, 1, ["Chile", "Torres", "Paine", "Torres Paine", "Torres del Paine"], OP5),
+option(4, "4) Volver", 0, 1, ["Regresar", "Salir", "Volver"], OP6),
+%Opciones segundo flujo Chatbot1
+option(1, "1) Central Park", 1, 2, ["Central", "Park", "Central Park"], OP7),
+option(2, "2) Museos", 1, 2, ["Museo"], OP8),
+option(3, "3) Ningún otro atractivo", 1, 3, ["Museo"], OP9),
+option(4, "4) Cambiar destino", 1, 1, ["Cambiar", "Volver", "Salir"], OP10),
+option(1, "1) Solo", 1, 3, ["Solo"], OP11),
+option(2, "2) En pareja", 1, 3, ["Pareja"], OP12),
+option(3, "3) En familia", 1, 3, ["Familia"], OP13),
+option(4, "4) Agregar más atractivos", 1, 2, ["Volver", "Atractivos"], OP14),
+option(5, "5) En realidad quiero otro destino", 1, 1, ["Cambiar destino"], OP15),
+flow(1, "Flujo 1 Chatbot1\n¿Dónde te Gustaría ir?", [OP3, OP4, OP5, OP6], F20),
+flow(2, "Flujo 2 Chatbot1\n¿Qué atractivos te gustaría visitar?", [OP7, OP8, OP9, OP10], F21),
+flow(3, "Flujo 3 Chatbot1\n¿Vas solo o acompañado?", [OP11, OP12, OP13, OP14, OP15], F22),
+chatbot(1, "Agencia Viajes",  "Bienvenido\n¿Dónde quieres viajar?", 1, [F20, F21, F22], CB1),
+%Chatbot2
+option(1, "1) Carrera Técnica", 2, 1, ["Técnica"], OP16),
+option(2, "2) Postgrado", 2, 1, ["Doctorado", "Magister", "Postgrado"], OP17),
+option(3, "3) Volver", 0, 1, ["Volver", "Salir", "Regresar"], OP18),
+flow(1, "Flujo 1 Chatbot2\n¿Qué te gustaría estudiar?", [OP16, OP17, OP18], F30),
+chatbot(2, "Orientador Académico",  "Bienvenido\n¿Qué te gustaría estudiar?", 1, [F30], CB2),
+system("Chatbots Paradigmas", 0, [CB0], S0),
+% systemAddChatbot(S0, CB0, S1), %si esto se descomenta, debe dar false, porque es chatbot id duplicado.
+systemAddChatbot(S0, CB1, S01),
+systemAddChatbot(S01, CB2, S02),
+systemAddUser(S02, "user1", S2),
+systemAddUser(S2, "user2", S3),
+% systemAddUser(S3, "user2", S4), %si esto se descomenta, debe dar false, porque es username duplicado
+systemAddUser(S3, "user3", S5),
+% systemLogin(S5, "user8", S6), %si esto se descomenta, debe dar false ;user8 no existe.
+systemLogin(S5, "user1", S7),
+% systemLogin(S7, "user2", S8), %si esto se descomenta, debe dar false, ya hay usuario con login
+systemLogout(S7, S9),
+systemLogin(S9, "user2", S10).
+%systemTalkRec(S10, "hola", S11), %las comento porque no estan implementadas
+%systemTalkRec(S11, "1", S12),
+%systemTalkRec(S12, "1", S13),
+%systemTalkRec(S13, "Museo", S14),
+%systemTalkRec(S14, "1", S15),
+%systemTalkRec(S15, "3", S16),
+%systemTalkRec(S16, "5", S17),
+%systemSynthesis(S17, "user2", Str1),
+%systemSimulate(S02, 5, 32131, S99)
