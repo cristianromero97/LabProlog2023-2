12e65b2bee404a8e5f52e53ee12434c805ec0782
cristianromero97 <cristian.romero.m@usach.cl>
2023-11-11 21:51:03 -0400

    Archivo Principal

---
+++ main_19800734_RomeroMartinez.pl
@@ -0,0 +1,27 @@
+% PARADIGMAS DE LA PROGRAMACIÓN
+% SECCIÓN DEL CURSO: 0-A-1
+% PROFESOR DE TEORÍA: EDMUNDO LEIVA
+% PROFESOR DE LABORATORIO: EDMUNDO LEIVA
+%
+% AUTOR
+% NOMBRE: Cristian Alexis Romero Martinez
+% RUT: 19.800.734-k 
+% CARRERA: Ingenieria en Ejecucion en Informatica
+% VERSIÓN SWI-PROLOG: 9.0
+
+%HECHOS
+
+%REGLAS
+
+%PREDICADOS
+
+%IMPORTACION 
+%INSTRUCCIONES : Este archivo importa todos los demas tdas y puede usarse para la ejecucion de pruebas con el script de pruebas y ejemplos.
+%En caso de que este archivo falle, se puede utilizar el archivo General, dicho archivo contiene todo el programa completo.
+:-include(tda_ChatHistory_19800734_RomeroMartinez).
+:-include(tda_Chatbot_19800734_RomeroMartinez).
+:-include(tda_User_19800734_RomeroMartinez).
+:-include(tda_Option_19800734_RomeroMartinez).
+:-include(tda_Flow_19800734_RomeroMartinez).
+:-include(tda_System_19800734_RomeroMartinez).
+
